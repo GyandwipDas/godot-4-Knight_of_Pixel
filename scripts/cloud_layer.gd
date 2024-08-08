9ea5fb2a5d7cca09cfc9f5e0bc939ec3335481d6
GyandwipDas <dasgyandwip@gmail.com>
2024-08-08 18:46:10 +0530

    added parallax bg

---
+++ scripts/cloud_layer.gd
@@ -0,0 +1,7 @@
+extends ParallaxLayer
+
+@export var cloud_speed = -5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	motion_offset.x += cloud_speed * delta
